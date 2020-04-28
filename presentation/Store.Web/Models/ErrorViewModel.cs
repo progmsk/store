8b033df0d425ea8eb8134f5b15a62e21e115ad48
Mark Shevchenko <markshevchenko@gmail.com>
2020-04-28 20:16:55 +0300

    Создать веб-приложение

---
+++ presentation/Store.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Store.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
