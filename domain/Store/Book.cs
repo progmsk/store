ab4d03b154c0e2899dd937031395c0f31d6339cd
Mark Shevchenko <markshevchenko@gmail.com>
2020-04-28 19:45:58 +0300

    Описать сущность Book

---
+++ domain/Store/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Store
+{
+    public class Book
+    {
+        public int Id { get; }
+
+        public string Title { get; }
+
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
