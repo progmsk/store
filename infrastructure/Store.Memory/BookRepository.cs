0e7a2f1308a4dee9fe8eba7aea5756dcb0ac7160
Mark Shevchenko <markshevchenko@gmail.com>
2020-04-28 20:11:08 +0300

    Добавить реализацию репозитория книг в памяти

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art Of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language"),
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                        .ToArray();
+        }
+    }
+}
