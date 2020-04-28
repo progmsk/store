5e15a6ab30bf0e5cac4a5db9cb2d49bd7b408bea
Mark Shevchenko <markshevchenko@gmail.com>
2020-04-28 20:33:59 +0300

    Добавить страницу поиска

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepository bookRepository;
+
+        public SearchController(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var books = bookRepository.GetAllByTitle(query);
+
+            return View(books);
+        }
+    }
+}
--- presentation/Store.Web/Startup.cs
+++ presentation/Store.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Store.Memory;
 
 namespace Store.Web
 {
@@ -24,6 +25,7 @@ namespace Store.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<IBookRepository, BookRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
