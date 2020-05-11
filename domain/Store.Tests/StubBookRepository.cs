dc44e709371acbdff5fa215b931f397e0dab3cb1
Mark Shevchenko <markshevchenko@gmail.com>
2020-05-11 10:20:57 +0300

    Добавил StubBookRepository

---
+++ domain/Store.Tests/StubBookRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace Store.Tests
+{
+    class StubBookRepository : IBookRepository
+    {
+        public Book[] ResultOfGetAllByIsbn { get; set; }
+
+        public Book[] ResultOfGetAllByTitleOrAuthor { get; set; }
+
+        public Book[] GetAllByIsbn(string isbn)
+        {
+            return ResultOfGetAllByIsbn;
+        }
+
+        public Book[] GetAllByTitleOrAuthor(string titleOrAuthor)
+        {
+            return ResultOfGetAllByTitleOrAuthor;
+        }
+    }
+}
