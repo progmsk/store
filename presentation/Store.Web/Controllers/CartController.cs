a7ff1615c1326fdaac3ab2a647eeaa1b85ec9a5c
Mark Shevchenko <markshevchenko@gmail.com>
2020-05-12 20:58:52 +0300

    Добавить книгу в корзину #5

---
+++ presentation/Store.Web/Controllers/CartController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Web.Models;
+
+namespace Store.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IBookRepository bookRepository;
+
+        public CartController(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+        public IActionResult Add(int id)
+        {
+            var book = bookRepository.GetById(id);
+            Cart cart;
+            if (!HttpContext.Session.TryGetCart(out cart))
+                cart = new Cart();
+
+            if (cart.Items.ContainsKey(id))
+                cart.Items[id]++;
+            else
+                cart.Items[id] = 1;
+
+            cart.Amount += book.Price;
+
+            HttpContext.Session.Set(cart);
+
+            return RedirectToAction("Index", "Book", new { id });
+        }
+    }
+}
---
+++ presentation/Store.Web/SessionExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Store.Web.Models;
+using System.IO;
+using System.Text;
+
+namespace Store.Web
+{
+    public static class SessionExtensions
+    {
+        private const string key = "Cart";
+
+        public static void Set(this ISession session, Cart value)
+        {
+            if (value == null)
+                return;
+
+            using (var stream = new MemoryStream())
+            using (var writer = new BinaryWriter(stream, Encoding.UTF8, true))
+            {
+                writer.Write(value.Items.Count);
+
+                foreach (var item in value.Items)
+                {
+                    writer.Write(item.Key);
+                    writer.Write(item.Value);
+                }
+
+                writer.Write(value.Amount);
+
+                session.Set(key, stream.ToArray());
+            }
+        }
+
+        public static bool TryGetCart(this ISession session, out Cart value)
+        {
+            if (session.TryGetValue(key, out byte[] buffer))
+            {
+                using (var stream = new MemoryStream(buffer))
+                using (var reader = new BinaryReader(stream, Encoding.UTF8, true))
+                {
+                    value = new Cart();
+
+                    var length = reader.ReadInt32();
+                    for (int i = 0; i < length; i++)
+                    {
+                        var bookId = reader.ReadInt32();
+                        var count = reader.ReadInt32();
+
+                        value.Items.Add(bookId, count);
+                    }
+
+                    value.Amount = reader.ReadDecimal();
+
+                    return true;
+                }
+            }
+
+            value = null;
+            return false;
+        }
+    }
+}
