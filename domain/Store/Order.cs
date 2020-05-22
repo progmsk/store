877d9428ff5bbe305f759d7a608e758fdfd7a3bb
Vadim <gitpublicmail@yandex.ru>
2020-05-22 21:19:31 +0300

    Добавить методы для удаления книг из корзины

--- domain/Store/Order.cs
+++ domain/Store/Order.cs
@@ -9,6 +9,7 @@ namespace Store
         public int Id { get; }
 
         private List<OrderItem> items;
+
         public IReadOnlyCollection<OrderItem> Items
         {
             get { return items; }
@@ -50,6 +51,40 @@ namespace Store
                 items.Remove(item);
                 items.Add(new OrderItem(book.Id, item.Count + count, book.Price));
             }
+        }
+
+        public void RemoveItem(Book book, int count)
+        {
+            if (book == null)
+                throw new ArgumentNullException(nameof(book));
+
+            if (items.Count == 0)
+                throw new InvalidOperationException("Cart must contain items");
+
+            var item = items.SingleOrDefault(x => x.BookId == book.Id);
+            if (item == null)
+                throw new InvalidOperationException("Cart does not contain item with ID: " + book.Id);
+
+            items.Remove(item);
+            if (item.Count - count == 0)
+                return;
+            
+            items.Add(new OrderItem(book.Id, item.Count - count, book.Price));
+            }
+
+        public void RemoveItems(Book book)
+        {
+            if (book == null)
+                throw new ArgumentNullException(nameof(book));
+
+            if (items.Count == 0)
+                throw new InvalidOperationException("Cart must contain items");
+
+            var item = items.SingleOrDefault(x => x.BookId == book.Id);
+            if (item == null)
+                throw new InvalidOperationException("Cart does not contain item with ID: " + book.Id);
+
+            items.RemoveAll(x => x.BookId == book.Id);
         }
     }
 }
--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Store.Web.Models;
 
@@ -74,8 +75,56 @@ namespace Store.Web.Controllers
             cart.TotalCount = order.TotalCount;
             cart.TotalPrice = order.TotalPrice;
             HttpContext.Session.Set(cart);
-
+            
             return RedirectToAction("Index", "Book", new { id });
+        }
+
+        public IActionResult RemoveItem(int id)
+        {
+            Order order;
+            Cart cart;
+            if (HttpContext.Session.TryGetCart(out cart))
+            {
+                order = orderRepository.GetById(cart.OrderId);
+            }
+            else
+            {
+                order = orderRepository.Create();
+                cart = new Cart(order.Id);
+            }
+
+            var book = bookRepository.GetById(id);
+            order.RemoveItem(book, 1);
+            orderRepository.Update(order);
+
+            cart.TotalCount = order.TotalCount;
+            cart.TotalPrice = order.TotalPrice;
+            HttpContext.Session.Set(cart);
+            return RedirectToAction("Index", "Book", new { id });
+        }
+
+        public IActionResult RemoveItems(int id)
+        {
+            Order order;
+            Cart cart;
+            if (HttpContext.Session.TryGetCart(out cart))
+            {
+                order = orderRepository.GetById(cart.OrderId);
+            }
+            else
+            {
+                order = orderRepository.Create();
+                cart = new Cart(order.Id);
+            }
+
+            var book = bookRepository.GetById(id);
+            order.RemoveItems(book);
+            orderRepository.Update(order);
+
+            cart.TotalCount = order.TotalCount;
+            cart.TotalPrice = order.TotalPrice;
+            HttpContext.Session.Set(cart);
+            return RedirectToAction("Index", "Book", new {id});
         }
     }
 }
