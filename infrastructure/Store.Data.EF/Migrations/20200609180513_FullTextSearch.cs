90d41eee3f1501084060892e7a47f2df501c3b3f
Mark Shevchenko <markshevchenko@gmail.com>
2020-06-09 21:08:19 +0300

    Добавить полнотекстовый поиск

---
+++ infrastructure/Store.Data.EF/Migrations/20200609180513_FullTextSearch.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Store.Data.EF.Migrations
+{
+    public partial class FullTextSearch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE FULLTEXT CATALOG StoreFullTextCatalog AS DEFAULT", suppressTransaction: true);
+            migrationBuilder.Sql("CREATE FULLTEXT INDEX ON Books(Author, Title) KEY INDEX PK_Books WITH STOPLIST = SYSTEM", suppressTransaction: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FULLTEXT INDEX ON Books", suppressTransaction: true);
+            migrationBuilder.Sql("DROP FULLTEXT CATALOG StoreFullTextCatalog", suppressTransaction: true);
+        }
+    }
+}
