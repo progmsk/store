fc52d470845d63ee84b3111c9f1d659cfe2dd4e0
Mark Shevchenko <markshevchenko@gmail.com>
2020-06-25 15:52:24 +0300

    Подготовить проект к уроку

--- domain/Store/Messages/DebugNotificationService.cs
+++ domain/Store/Messages/DebugNotificationService.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.Net.Mail;
-using System.Text;
+﻿using System.Diagnostics;
 
 namespace Store.Messages
 {
@@ -14,21 +11,9 @@ namespace Store.Messages
 
         public void StartProcess(Order order)
         {
-            using (var client = new SmtpClient())
-            {
-                var message = new MailMessage("from@at.my.domain", "to@at.my.domain");
-                message.Subject = "Заказ #" + order.Id;
-
-                var builder = new StringBuilder();
-                foreach (var item in order.Items)
-                {
-                    builder.Append("{0}, {1}", item.BookId, item.Count);
-                    builder.AppendLine();
-                }
-
-                message.Body = builder.ToString();
-                client.Send(message);
-            }
+            Debug.WriteLine("Order ID {0}", order.Id);
+            Debug.WriteLine("Delivery: {0}", (object)order.Delivery.Description);
+            Debug.WriteLine("Payment: {0}", (object)order.Payment.Description);
         }
     }
 }
